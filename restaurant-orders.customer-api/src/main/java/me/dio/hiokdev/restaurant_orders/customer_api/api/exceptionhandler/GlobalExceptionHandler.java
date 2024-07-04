5581738d28956292a960962ccd74f01446ff00bd
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-07-04 15:28:40 -0300

    feat: customer-service: add customer repository interface and project structure

---
+++ restaurant-orders.customer-api/src/main/java/me/dio/hiokdev/restaurant_orders/customer_api/api/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,4 @@
+package me.dio.hiokdev.restaurant_orders.customer_api.api.exceptionhandler;
+
+public class GlobalExceptionHandler {
+}
---
+++ restaurant-orders.customer-api/src/main/java/me/dio/hiokdev/restaurant_orders/customer_api/domain/exception/ResourceNotFoundException.java
@@ -0,0 +1,4 @@
+package me.dio.hiokdev.restaurant_orders.customer_api.domain.exception;
+
+public class ResourceNotFoundException {
+}
