9269bce8f73796eebebd31d7191aa88245e0f3d7
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-07-04 15:45:33 -0300

    feat: customer-service: add global exception handler

--- restaurant-orders.customer-api/src/main/java/me/dio/hiokdev/restaurant_orders/customer_api/api/dto/ErrorDetails.java
+++ restaurant-orders.customer-api/src/main/java/me/dio/hiokdev/restaurant_orders/customer_api/api/dto/ErrorDetails.java
@@ -1,4 +1,17 @@
 package me.dio.hiokdev.restaurant_orders.customer_api.api.dto;
 
-public record ErrorDetails() {
+import lombok.Builder;
+
+import java.time.OffsetDateTime;
+
+public record ErrorDetails(
+        Integer status,
+        String message,
+        String details,
+        OffsetDateTime timestamp
+) {
+
+    @Builder
+    public ErrorDetails {}
+
 }
--- restaurant-orders.customer-api/src/main/java/me/dio/hiokdev/restaurant_orders/customer_api/api/exceptionhandler/GlobalExceptionHandler.java
+++ restaurant-orders.customer-api/src/main/java/me/dio/hiokdev/restaurant_orders/customer_api/api/exceptionhandler/GlobalExceptionHandler.java
@@ -1,4 +1,53 @@
 package me.dio.hiokdev.restaurant_orders.customer_api.api.exceptionhandler;
 
+import me.dio.hiokdev.restaurant_orders.customer_api.api.dto.ErrorDetails;
+import me.dio.hiokdev.restaurant_orders.customer_api.domain.exception.CustomerException;
+import me.dio.hiokdev.restaurant_orders.customer_api.domain.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.OffsetDateTime;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> resourceNotFoundExceptionHandler(ResourceNotFoundException ex, WebRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        var errorDetails = ErrorDetails.builder()
+                .status(status.value())
+                .message(ex.getMessage())
+                .details(request.getDescription(false))
+                .timestamp(OffsetDateTime.now())
+                .build();
+        return new ResponseEntity<>(errorDetails, status);
+    }
+
+    @ExceptionHandler(CustomerException.class)
+    public ResponseEntity<ErrorDetails> customerExceptionHandler(CustomerException ex, WebRequest request) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        var errorDetails = ErrorDetails.builder()
+                .status(status.value())
+                .message(ex.getMessage())
+                .details(request.getDescription(false))
+                .timestamp(OffsetDateTime.now())
+                .build();
+        return new ResponseEntity<>(errorDetails, status);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> globalExceptionHandler(Exception ex, WebRequest request) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        var errorDetails = ErrorDetails.builder()
+                .status(status.value())
+                .message(ex.getMessage())
+                .details(request.getDescription(false))
+                .timestamp(OffsetDateTime.now())
+                .build();
+        return new ResponseEntity<>(errorDetails, status);
+    }
+
 }
