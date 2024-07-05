b93c5d6fa7bdfdc4e2803b840648e659396f8d00
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-07-04 23:05:22 -0300

    feat: order-service: add order domain model

---
+++ restaurant-orders.order-service/src/main/java/me/dio/hiokdev/restaurant_orders/order_service/domain/model/Order.java
@@ -0,0 +1,43 @@
+package me.dio.hiokdev.restaurant_orders.order_service.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "customer_id", nullable = false)
+    private UUID customerId;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private long value;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private OffsetDateTime createdAt;
+
+}
