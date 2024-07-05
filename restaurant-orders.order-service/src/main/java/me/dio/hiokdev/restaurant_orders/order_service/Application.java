0582376839cc707309dba2dc1d5eab0f557fc492
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-07-04 22:56:45 -0300

    feat: order-service: config eureka client order-service

--- restaurant-orders.order-service/src/main/java/me/dio/hiokdev/restaurant_orders/order_service/Application.java
+++ restaurant-orders.order-service/src/main/java/me/dio/hiokdev/restaurant_orders/order_service/Application.java
@@ -2,7 +2,9 @@ package me.dio.hiokdev.restaurant_orders.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
