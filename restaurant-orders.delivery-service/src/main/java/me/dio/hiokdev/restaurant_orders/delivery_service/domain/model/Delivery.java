dfc428b3c8fb3543a1b0e23aba13abf578180a2f
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-07-05 13:17:18 -0300

    feat: delivery-service: add delivery model

---
+++ restaurant-orders.delivery-service/src/main/java/me/dio/hiokdev/restaurant_orders/delivery_service/domain/model/Delivery.java
@@ -0,0 +1,40 @@
+package me.dio.hiokdev.restaurant_orders.delivery_service.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "deliveries")
+public class Delivery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "order_id", nullable = false)
+    private UUID orderId;
+
+    @Column(name = "customer_id", nullable = false)
+    private UUID customerId;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(name = "shipping_value", nullable = false)
+    private long shippingValue;
+
+}
